fcb05ef5ead84b93556214615bbd09f6b2d0325e
Jules Fournier <jules.fournier.1210@gmail.com>
2020-10-21 15:39:50 +0200

    init

---
+++ pays.js
@@ -0,0 +1,116 @@
+const pays = [
+  {
+    nom: 'Comores',
+    flag: 'f79f878234-235.png',
+  },
+  {
+    nom: 'Trinité-et-Tobago',
+    flag: 'f79f878290-235.png',
+  },
+  {
+    nom: 'Mauritanie',
+    flag: 'faee028f17-235.png',
+  },
+  {
+    nom: 'Salvador',
+    flag: '07eae2eede-235.png',
+  },
+  {
+    nom: 'Érythrée',
+    flag: '7b1e707f87-235.png',
+  },
+  {
+    nom: 'Bénin',
+    flag: 'd7bfd7e30c-235.png',
+  },
+  {
+    nom: 'Lesotho',
+    flag: '318d929f96-235.png',
+  },
+  {
+    nom: 'Rwanda',
+    flag: '5420b7c7fd-235.png',
+  },
+  {
+    nom: 'Grenade',
+    flag: 'bdc50b941e-235.png',
+  },
+  {
+    nom: 'Guinée',
+    flag: 'f51d8d2fc6-235.png',
+  },
+  {
+    nom: 'Guinée équatoriale',
+    flag: 'cbc5aa8b02-235.png',
+  },
+  {
+    nom: 'Saint-Christophe-et-Niévès',
+    flag: '908a05646d-235.png',
+  },
+  {
+    nom: 'Sao Tomé-et-Principe',
+    flag: 'dafbba6207-235.png',
+  },
+  {
+    nom: 'Guinée-Bissau',
+    flag: 'bc470f83b8-235.png',
+  },
+  {
+    nom: 'Haïti',
+    flag: '5aeb7fcfbf-235.png',
+  },
+  {
+    nom: 'Bahamas',
+    flag: '2f2e434c29-235.png',
+  },
+  {
+    nom: 'Antigua-et-Barbuda',
+    flag: '614ce70209-235.png',
+  },
+  {
+    nom: 'Kiribati',
+    flag: 'e23977ce6f-235.png',
+  },
+  {
+    nom: 'Botswana',
+    flag: 'f593b19054-235.png',
+  },
+  {
+    nom: 'Nauru',
+    flag: 'aec64cee55-235.png',
+  },
+  {
+    nom: 'Guyana',
+    flag: '04f700b893-235.png',
+  },
+  {
+    nom: 'Vanuatu',
+    flag: '88887f018f-235.png',
+  },
+  {
+    nom: 'Dominique',
+    flag: 'cca6966b19-235.png',
+  },
+  {
+    nom: 'Suriname',
+    flag: '21972cc877-235.png',
+  },
+  {
+    nom: 'Timor oriental',
+    flag: '2820018675-235.png',
+  },
+  {
+    nom: 'Bolivie',
+    flag: 'e628f910cd-235.png',
+  },
+  {
+    nom: 'Gambie',
+    flag: '08c6fc6253-235.png',
+  },
+  {
+    nom: 'Salomon',
+    flag: '7a0483d690-235.png',
+  },
+]
+
+module.exports = pays
